09754c041cba3590b3c315d14932dcdbc2fce788
SeanTheShe3p <swiddowson@students.mchenry.edu>
2022-10-10 18:36:29 -0500

    Initial commit

---
+++ src/main/kotlin/Commercial.kt
@@ -0,0 +1,35 @@
+import java.lang.reflect.Field
+import java.text.DecimalFormat
+import java.text.Format
+import java.text.Normalizer.Form
+
+class Comercial(propertyName:String, customerName:String, customerPhone:String, customerAddress:String, squareFootage:Double, numberOfProperties:Int): Customer(customerName,customerPhone,customerAddress, squareFootage){
+    var propertyName= ""
+    var customerName = ""
+    var customerPhone = ""
+    var customerAddress = ""
+    var squareFootage = 0.0
+    var numberOfProperties = 0
+    init {
+        this.propertyName = propertyName
+        this.customerName = customerName
+        this.customerPhone = customerPhone
+        this.customerAddress = customerAddress
+        this.squareFootage = squareFootage
+        this.numberOfProperties = numberOfProperties
+    }
+    fun calcRate(){
+        var totalcost: Double = 0.0
+        val dec = DecimalFormat("\$###,###,###.00")
+        if (this.numberOfProperties==1){
+            totalcost = (this.squareFootage/1000)*5
+            var finalcost = dec.format(totalcost)
+            println("Commercial Property "+this.propertyName+"\nContact: "+this.customerName+"\n"+customerPhone+"\n"+customerAddress+"\n\nat a rate of $5 per 1000 sq. ft, the charge will be "+finalcost)
+        }
+        else if (this.numberOfProperties >=2){
+            totalcost = ((this.squareFootage/1000)*5)*.9
+            var finalcost = dec.format(totalcost)
+            println("Commercial Property "+this.propertyName+"\nContact: "+this.customerName+"\n"+customerPhone+"\n"+customerAddress+"\n\nat a rate of $5 per 1000 sq. ft, with a 10% discount, the charge will be "+finalcost)
+        }
+    }
+}
---
+++ src/main/kotlin/Customer.kt
@@ -0,0 +1,8 @@
+open class Customer(customerName: String, customerPhone: String, customerAddress:String, squareFootage:Double){
+    init {
+        var customerName = customerName
+        var customerPhone = customerPhone
+        var customerAddress = customerAddress
+        var squareFootage = squareFootage
+    }
+}
